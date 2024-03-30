8f8cb9460d396f2aedaae88ea73786bd6e861632
Nabin Khanal <nabinkhanal@skiff.com>
2024-03-30 23:02:06 +0545

    incomplete: bugs

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// Uncomment this line to pass the first stage
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -14,61 +15,120 @@ import (
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 
-func decodeBencode(bencodedString string) (interface{}, string, error) {
-	if bencodedString[0] == 'l' {
-		var elements []interface{} = []interface{}{}
-		remaining := bencodedString[1 : len(bencodedString)-1]
-		if remaining == "" {
-			return elements, "", nil
-		}
-		for {
-			element, rem, err := decodeBencode(remaining)
-			if err != nil {
-				return elements, "", nil
-			}
-			remaining = rem
-			elements = append(elements, element)
-			if remaining == "" {
-				break
-			}
-		}
-		return elements, "", nil
+func parseInt(input string) (int, int, error) {
 
-	} else if bencodedString[0] == 'i' {
-		value := ""
-		var i int
-		for i = 1; i < len(bencodedString); i++ {
-			if bencodedString[i] == 'e' {
-				break
-			}
-			value = value + string(bencodedString[i])
-		}
-		number, err := strconv.Atoi(value)
-		if err != nil {
-			return "", bencodedString[i+1:], err
-		}
-		return number, bencodedString[i+1:], nil
-	} else if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", bencodedString[firstColonIndex+1+length:], err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], bencodedString[firstColonIndex+1+length:], nil
-	} else {
-		return "", "", fmt.Errorf("Only strings are supported at the moment")
+	value := ""
+	if len(input) == 0 {
+		return -1, -1, errors.New("Cannot parse empty string.")
 	}
+	current := 1
+	for {
+		if input[current] == 'e' {
+			break
+		}
+		value = value + string(input[current])
+		current++
+	}
+	number, err := strconv.Atoi(value)
+	if err != nil {
+		return -1, -1, err
+	}
+	return number, current + 1, nil
+}
+func parseString(input string) (string, int, error) {
+
+	var firstColonIndex int
+
+	if len(input) == 0 {
+		return "", -1, errors.New("Cannot parse empty string.")
+	}
+	for current := 0; current < len(input); current++ {
+		if input[current] == ':' {
+			firstColonIndex = current
+			break
+		}
+	}
+
+	lengthStr := input[:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", -1, err
+	}
+
+	return input[firstColonIndex+1 : firstColonIndex+1+length], firstColonIndex + 1 + length, nil
+}
+
+func decodeBencode(bencodedString string) (interface{}, error) {
+
+	if bencodedString[0] == 'l' {
+		bencodedString = bencodedString[1 : len(bencodedString)-1]
+		currentIndex := 0
+		result := []interface{}{}
+		s := []interface{}{}
+		var oldTop *[]interface{} = nil
+		current := &result
+		s = append(s, &result)
+		for currentIndex < len(bencodedString) {
+			if bencodedString[currentIndex] == 'l' {
+				fmt.Println("Start:", bencodedString[currentIndex:])
+				newList := []interface{}{}
+				s = append(s, &newList)
+				oldTop = current
+				current = &newList
+				currentIndex++
+			} else if bencodedString[currentIndex] == 'e' {
+				fmt.Println("End:", bencodedString[currentIndex:])
+				fmt.Println("Result", result, "OldTop", *oldTop, "current", *current)
+				*oldTop = append(*oldTop, *current)
+				current = oldTop
+				fmt.Println("Result", result, "OldTop", *oldTop, "current", *current)
+				s = s[:len(s)-1]
+				if len(s) == 0 {
+					oldTop = nil
+				} else {
+					oldTop = s[len(s)-1].(*[]interface{})
+				}
+				currentIndex++
+			} else if bencodedString[currentIndex] == 'i' {
+				intValue, index, err := parseInt(bencodedString[currentIndex:])
+				if err != nil {
+					return nil, err
+				}
+				currentIndex = currentIndex + index
+				*current = append(*current, intValue)
+			} else if unicode.IsDigit(rune(bencodedString[currentIndex])) {
+				strValue, index, err := parseString(bencodedString[currentIndex:])
+				if err != nil {
+					return nil, err
+				}
+				currentIndex = currentIndex + index
+				*current = append(*current, strValue)
+			} else {
+				fmt.Println(bencodedString[currentIndex:])
+			}
+		}
+		return result, nil
+	} else if bencodedString[0] == 'd' {
+		// elements := map[string]interface{}{}
+		// bencodedString = bencodedString[1 : len(bencodedString)-1]
+	} else if bencodedString[0] == 'i' {
+		intValue, _, err := parseInt(bencodedString)
+		if err != nil {
+			return nil, err
+		}
+		return intValue, nil
+	} else if unicode.IsDigit(rune(bencodedString[0])) {
+		strValue, _, err := parseString(bencodedString)
+		if err != nil {
+			return nil, err
+		}
+		return strValue, nil
+
+	} else {
+		return nil, errors.New("Unsupported")
+	}
+	return 3, nil
 }
 
 func main() {
@@ -79,7 +139,7 @@ func main() {
 
 		bencodedValue := os.Args[2]
 
-		decoded, _, err := decodeBencode(bencodedValue)
+		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
